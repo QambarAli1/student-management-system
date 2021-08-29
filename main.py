843eb1ccf2e227923247c9ab1c78afd598b6576c
QambarAli1 <qamberali4904@gmail.com>
2021-08-29 23:01:13 +0500

    updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 from tkinter.constants import GROOVE
 from tkinter.font import BOLD
 
@@ -12,6 +13,10 @@ win.title("Student Mangement SYstem")
 title_label = tk.Label(win,text="Student Management System",font=("Ariel",25,BOLD),border=12,relief=tk.GROOVE,foreground="yellow",bg="blue")
 title_label.pack(side=tk.TOP,fill=tk.X)
 
-detail_frame = tk.LabelFrame()
+detail_frame = tk.LabelFrame(win,text="Enter Details",font=("Arial",20,BOLD),bd=12,relief=tk.GROOVE)
+detail_frame.place(x=20,y=80,width=420,height=500)
+
+data_frame = tk.LabelFrame(win,text="Data",font=("Arial",20,BOLD),bd=12,relief=tk.GROOVE)
+data_frame.place(x=500,y=80,width=700,height=500)
 
 win.mainloop()
